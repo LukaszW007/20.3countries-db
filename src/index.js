bc913f372d94dafd54879c41d16978a1a24689d6
LukaszWiszniewski <wisznu07@gmail.com>
2018-04-11 20:24:15 +0200

    Added routes.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Router, hashHistory } from 'react-router';
-import { Provider } from 'react-redux';
+import {render} from 'react-dom';
+import {Router, hashHistory} from 'react-router';
+import {Provider} from 'react-redux';
 import store from './store';
 import DevTools from './DevTools'
 import routes from './routes';
 
 render(
     <Provider store={store}>
-        <Router history={hashHistory} routes={routes}>
-        </Router>
-        <DevTools />
+        <div>
+            <Router history={hashHistory} routes={routes}>
+            </Router>
+            <DevTools/>
+        </div>
     </Provider>,
     document.getElementById('root')
 );
